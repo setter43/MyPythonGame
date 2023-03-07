c1f43e5ccc0dbae67aa3022958faccf74e6dbd3d
Ben Taylor <ben.taylor@cubic.com>
2023-03-07 16:19:04 +0000

    Improved Sprite Movement

--- PyKeyboardInp.py
+++ PyKeyboardInp.py
@@ -1,11 +1,13 @@
 import pygame
 from pygame.locals import* #imports all key code
 
-pygame.init()#initialize game window
+#initialize game window
+pygame.init()
 
 game_height = 700
 game_width = 700
-screen = pygame.display.set_mode((game_width, game_height), 0, 32)#create game window
+#create game window
+screen = pygame.display.set_mode((game_width, game_height), 0, 32)
 
 #import sprite
 footballSprite = pygame.image.load('./img/football.png')
@@ -19,21 +21,28 @@ game_over = False
 
 x, y = (0, 0)
 
+# Define movement speed
+MOVEMENT_SPEED = 0.5
+
 #game loop
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
         pressed = pygame.key.get_pressed()
-    if pressed[K_UP]:#Standing for Key Press is UP Arrow
-            y -= 0.5
+
+    # Update position based on pressed keys
+    if pressed[K_UP]:
+        y -= MOVEMENT_SPEED
     if pressed[K_DOWN]:
-        y += 0.5
+        y += MOVEMENT_SPEED
     if pressed[K_LEFT]:
-        x -= 0.5
+        x -= MOVEMENT_SPEED
     if pressed[K_RIGHT]:
-        x += 0.5
-    screen.fill((0,0,0))#clears game screens
+        x += MOVEMENT_SPEED
+    #clears game screens
+    screen.fill((0,0,0))
+
     screen.blit(footballSprite,(x, y))
     pygame.display.update()
 pygame.quit()
